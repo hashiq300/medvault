c7f375dc258f01a3a2180c676b95c3052b72dad4
hashiq300 <mohdhashique10@gmail.com>
2024-02-03 21:29:17 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <div>
+    <h1>hello from medvault</h1>
+    <Button>Submit</Button>
+  </div>;
+}
+
+export default App;
