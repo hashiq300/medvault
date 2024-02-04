cd9a8ac7be518f060c9e1b2ca6dbf9509fe387bb
Antony Jaison <antonyjaison639@gmail.com>
2024-02-04 20:34:00 +0530

    added progress bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import InsightSection from "@/components/InsightSection";
 import MedicationIntake from "@/components/MedicationIntake";
+import ProgressBar from "@/components/ProgressBar";
 
 function HomePage() {
   return (
-    <div className="px-[1.4rem] pt-4">
+    <div className="px-[1.4rem] pb-40">
       <div className="space-y-6">
+        <ProgressBar/>
         <MedicationIntake />
         <InsightSection />
       </div>
