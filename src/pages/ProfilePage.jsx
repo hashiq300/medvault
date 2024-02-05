6ef799862c4453ab9aad372efd3359c84c40eed6
Antony Jaison <antonyjaison639@gmail.com>
2024-02-05 18:12:43 +0530

    added profile details

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import BottomNavigation from "@/components/BottomNavigation";
 import { Pen, Trash, HelpCircle } from "lucide-react";
+import userStore from "@/store/userStore";
 
 const ProfilePage = () => {
+  const { user } = userStore();
+  console.log(user);
+
   return (
     <>
       <BottomNavigation />
@@ -14,27 +18,32 @@ const ProfilePage = () => {
         />
         <img
           className=" w-28 h-28 rounded-full object-cover absolute bottom-[-55px]"
-          src="https://github.com/shadcn.png"
+          src={user.photoURL}
           alt="avatar"
         />
       </div>
 
       <div className="pt-16 px-4 pb-44 flex flex-col gap-12">
         <div>
-          <h3 className=" text-center text-2xl font-medium">Jessica Jones</h3>
+          <h3 className=" text-center text-2xl font-medium">
+            {user.displayName}
+          </h3>
           <p className=" text-center text-[#707070] text-lg mt-3">
-            22 F | TX, USA
+            {/* 22 F | TX, USA */}
           </p>
           <p className=" text-center text-[#707070] text-lg mt-3">
-            jjones@outlook.com | +1 (323) 234 3423
+            {/* jjones@outlook.com | +1 (323) 234 3423 */}
+            {`${user.email} `}
           </p>
         </div>
 
         <div className=" flex flex-col gap-4">
-          <button className="w-full flex items-center gap-5 bg-[#1C1E27] p-5 rounded-2xl">
-            <Pen size={30} />
-            <p>Edit profile</p>
-          </button>
+          <a href="/edit-profile">
+            <button className="w-full flex items-center gap-5 bg-[#1C1E27] p-5 rounded-2xl">
+              <Pen size={30} />
+              <p>Edit profile</p>
+            </button>
+          </a>
 
           <button className="w-full flex items-center gap-5 bg-[#1C1E27] p-5 rounded-2xl">
             <HelpCircle size={30} />
