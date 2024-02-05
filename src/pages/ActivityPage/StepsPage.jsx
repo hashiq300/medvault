e424c4b78a3e3695097168a36ba5a04f0e9f96ad
Antony Jaison <antonyjaison639@gmail.com>
2024-02-05 15:11:37 +0530

    added steps data

--- src/pages/ActivityPage/StepsPage.jsx
+++ src/pages/ActivityPage/StepsPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import PageTitle from "@/components/PageTitle";
+import React, { useEffect, useState } from "react";
+import stepsData from "@/data/steps.json";
 
 const StepsPage = () => {
-  return (
-    <div>StepsPage</div>
-  )
-}
+  let totalSteps = 0;
+  let dailySteps = []; // Array to hold the total steps per day
+  let labels = []; // Array to hold the labels for the graph (e.g., dates)
 
-export default StepsPage
+  stepsData.bucket.forEach((bucket) => {
+    let stepsPerDay = 0;
+    bucket.dataset.forEach((dataset) => {
+      dataset.point.forEach((point) => {
+        stepsPerDay += point.value[0].intVal; // Assuming each `point` has a step count
+      });
+    });
+    totalSteps += stepsPerDay;
+
+    // Convert startTimeMillis to a readable date format for labels
+    const date = new Date(
+      parseInt(bucket.startTimeMillis)
+    ).toLocaleDateString();
+    labels.push(date);
+    dailySteps.push(stepsPerDay);
+  });
+
+  console.log(totalSteps,dailySteps,labels)
+
+  return (
+    <div className="px-4 pb-44">
+      <PageTitle title="Steps" />
+    </div>
+  );
+};
+
+export default StepsPage;
